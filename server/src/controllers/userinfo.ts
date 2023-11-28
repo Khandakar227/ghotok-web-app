1d72f0dddd1af6ea9b8291ddde49f5c6cd7f74be
Khandakar227 <xenonknight54@gmail.com>
2023-11-29 03:08:40 +0600

    Files structures setup

---
+++ server/src/controllers/userinfo.ts
@@ -0,0 +1,15 @@
+import {Request, Response} from 'express';
+
+class UserInfoRouteHandler {
+    constructor() {
+        
+    }
+    public static update(req:Request, res: Response) {
+
+    }
+    public static delete(req:Request, res: Response) {
+
+    }
+}
+
+export default UserInfoRouteHandler;
